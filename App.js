5531ab03b98d3f4cb55c0cae5922e08052ab465e
kireetb <bhat.kireet@gmail.com>
2024-05-01 14:05:35 -0400

    Add amplify custom authenticator component and Implement amplify auth

--- App.js
+++ App.js
@@ -1,52 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import { View } from "react-native";
+
 import Router from "./src/navigation/root";
 import * as Location from "expo-location";
 import "react-native-gesture-handler";
 
-export default function App() {
-  // const [location, setLocation] = useState(null);
-  // const [errorMsg, setErrorMsg] = useState(null);
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "./src/amplifyconfiguration.json";
+Amplify.configure(amplifyconfig);
 
-  // // This effect is triggered ONLY once when the app is started
-  // useEffect(() => {
-  //   (async () => {
-  //     let { status } = await Location.requestForegroundPermissionsAsync();
-  //     if (status !== "granted") {
-  //       setErrorMsg("Permission to access location was denied");
-  //       return;
-  //     }
+import { withAuthenticator } from "@aws-amplify/ui-react-native";
 
-  //     let location = await Location.getCurrentPositionAsync({});
-  //     setLocation(location);
-  //   })();
-  // }, []);
+function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-  // let text = "Waiting..";
-  // if (errorMsg) {
-  //   text = errorMsg;
-  // } else if (location) {
-  //   // text contains the json object as string which contains the location details
-  //   text = JSON.stringify(location);
-  //   console.log(text);
-  // }
+  // This effect is triggered ONLY once when the app is started
+  // Ask permission, only if location not enabled otherwise continue app
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+    console.log(text);
+  } else if (location) {
+    // text contains the json object as string which contains the location details
+    text = JSON.stringify(location);
+    console.log("Current Geolocation: ", text);
+  }
 
   return (
     <View style={{ flex: 1 }}>
       <StatusBar style="auto" />
-
-      {/* ## This has been built, just commented to test other components, however, not tied with other components */}
-      {/* <HomeScreen/>  */}
-
-      {/* ## This has been built, just commented to test other components, however, not tied with other components */}
-      {/* <DestinationSearch /> */}
-
-      {/* ## This has been built, just commented to test other components, however, not tied with other components */}
-      {/* <SearchResults/>   */}
-
       <Router />
     </View>
   );
 }
+
+export default withAuthenticator(App);
